dbc5cb37886a1db4304b812cb5b8f2df1dace1c0
Rikki Mongoose <rikki@rikkimongoose.ru>
2013-05-09 01:07:07 +0400

    fix the processor

--- kiyoaki/processor.py
+++ kiyoaki/processor.py
@@ -7,16 +7,39 @@ from random import choice
 class KBaseBlack:
     def __init__(self):
         self.sub_items = []
+        self.parent = None
 
-    def append(self, module):
-        if module is not None:
-            self.sub_items.append(module)
+    def append(self, sub_item):
+        if sub_item is not None:
+            self.sub_items.append(sub_item)
+            sub_item.parent = self
         return self
 
     def get_title(self):
         return None
 
-class KCodeBlock(KBaseBlock):
+    def get_parent(self):
+        return self.parent
+
+class KMethod(KCodeBlock):
+    def __init__(self, title):
+        KCodeBlock.__init__(self)
+        self.title=title
+
+    def get_title(self):
+        return self.title
+
+class KCommand():
+    def __init__(self, rule = None, return_mode = True, random_key = 0):
+        KCodeBlock.__init__(self, rule, return_mode, random_key)
+
+    def process(self, input_string):
+        if self.rule is None:
+            return input_string
+
+        return self.rule.apply(input_string)
+
+class KCodeBlock(KBaseBlock, KCommand):
     def __init__(self, rule = None, return_mode = True, random_key = 0):
         KBaseBlock.__init__(self)
         self.rule = rule
@@ -35,6 +58,31 @@ class KCodeBlock(KBaseBlock):
     def is_random_select(self, key):
         return self.random_key is not None and self.random_key > 0
 
+    def CreateBlock(self, rule, return_mode, random_key):
+        new_block = KCodeBlock(rule, return_mode, random_key)
+        self.append(new_block)
+        return new_block
+
+    def CreateCommand(self, rule, return_mode, random_key):
+        new_block = KCodeBlock(rule, return_mode, random_key)
+        self.append(new_block)
+        return self
+
+    def Block(self, rule = None, return_mode = True, random_key = 0):
+        return self.CreateBlock(rule, return_mode, random_key)
+
+    def Branch(self, rule = None, random_key = 0):
+        return self.CreateBlock(rule, True, random_key)
+
+    def Converter(self, rule):
+        return self.CreateCommand(rule, False, 0)
+
+    def Returner(self, rule):
+        return self.CreateCommand(rule, True, 0)
+
+    def End(self):
+        return new_block.get_parent()
+
     def process(self, input_string):
         blocks_to_exec = []
         for sub_item in sub_items:
@@ -52,24 +100,6 @@ class KCodeBlock(KBaseBlock):
 
         return input_string
 
-class KMethod(KCodeBlock):
-    def __init__(self, title):
-        KCodeBlock.__init__(self)
-        self.title=title
-
-    def get_title(self):
-        return self.title
-
-class KCommand(KCodeBlock):
-    def __init__(self, rule = None, return_mode = True, random_key = 0):
-        KCodeBlock.__init__(self, rule, return_mode, random_key)
-
-    def process(self, input_string):
-        if self.rule is None:
-            return input_string
-
-        return self.rule.apply(input_string)
-
 class KProcessorModulesPool(KBaseBlock):
     def __init__(self):
         KBaseBlock.__init__(self)
@@ -80,3 +110,28 @@ class KProcessorModulesPool(KBaseBlock):
             if module.get_title() == module_name:
                 return module_to_exec.process(input_string)
         return input_string
+
+class RuleValidator:
+    CMD_COMPARE_EQ = 1
+    CMD_COMPARE_NOTEQ = 2
+    def __init__(self, compare_command, comparation_set):
+        self.comparation_set = comparation_set
+        self.compare_command = compare_command
+
+    def _get_compare_command(self):
+        return self.compare_command
+
+    def test(self, input_string):
+        compare_command = self._get_compare_command()
+        for comparation_item in self.comparation_set:
+            if comparation_item.match(input_string):
+                return compare_command == self.CMD_COMPARE_EQ
+        return compare_command == self.CMD_COMPARE_NOTEQ
+
+    def replace(self, input_string, replacement):
+        if replacement is None:
+            replacement = ""
+        for comparation_item in self.comparation_set:
+            if comparation_item.match(input_string):
+                return re.sub(comparation_item,  "\\1%s\\2" % replacement, input_string)
+        return None
