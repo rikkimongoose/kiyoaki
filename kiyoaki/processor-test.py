2d9ccd46611bef764eff57b879fb4c7a93ddfc2f
Rikki Mongoose <rikki@rikkimongoose.ru>
2013-05-12 23:18:08 +0400

    test completed. fix some erratas

--- kiyoaki/processor-test.py
+++ kiyoaki/processor-test.py
@@ -6,54 +6,16 @@ import re
 
 from processor import *
 
-validator = RuleValidator(RuleValidator.CMD_COMPARE_EQ, [re.compile(r"(.*)ь()")])
-validatorL = RuleValidator(RuleValidator.CMD_COMPARE_EQ, [re.compile(r"()Л(.*)")])
-validator_branch1 = RuleValidator(RuleValidator.CMD_COMPARE_EQ, [re.compile(r"()Л(.*)")])
-validator_branch2 = RuleValidator(RuleValidator.CMD_COMPARE_EQ, [re.compile(r"()Н(.*)")])
-processor = TextProcessor(validator, "ин")
-processorN = TextProcessor(validatorL, "L", False)
-processorL = TextProcessor(validator, "ин", False)
-branch1 = ProcessBranch(validator_branch1)
-branch1.append(processor)
-branch2 = ProcessBranch(validator_branch2)
-branch2.append(processor)
-branch_block = ProcessBranchBlock()
-branch_block.append(branch1).append(branch2)
-process_module_line = ProcessModule("postfix_name_linear")
-process_module_line.append(processorN).append(processorL)
-process_module = ProcessModule("postfix_name")
-process_module.append(branch_block)
-processor_modules_pool = ProcessorModulesPool()
-processor_modules_pool.append(process_module)
 
 class ProcessorModulesPool(unittest.TestCase):
+    def testCreateBlock(self):
+        code_block = KCodeBlock()
+
     def testProcessing(self):
-        self.assertEqual(processor_modules_pool.process("Лень", "postfix_name"), "Ленин")
+        code_block = KCodeBlock()
+        code_block.Converter(RuleValidator(["-ь"], "ин"))
+        self.assertEqual(code_block.process("Лень"), "Ленин")
 
-class ProcessBranchBlockTest(unittest.TestCase):
-    def testProcessing(self):
-        branch_block.process("Лень")
-        self.assertTrue(branch_block.is_ready_to_return())
-        self.assertEqual(branch_block.get_value_to_return(), "Ленин")
-        branch_block.process("Нень")
-        self.assertTrue(branch_block.is_ready_to_return())
-        self.assertEqual(branch_block.get_value_to_return(), "Ненин")
-    
-    def testLineProcessing(self):
-        self.assertEqual(process_module_line.process("Лень"), "Lенин")
-
-class TextProcessor(unittest.TestCase):
-    def testProcessing(self):
-        processor.process("Лень")
-        self.assertTrue(processor.is_ready_to_return())
-        self.assertEqual(processor.get_value_to_return(), "Ленин")
-
-class RuleValidatorTest(unittest.TestCase):
-    def testTest(self):
-        self.assertTrue(validator.test("Лень"))
-
-    def testReplace(self):
-        self.assertEqual(validator.replace("Лень", "ин"), "Ленин")
 
 if __name__ == "__main__":
     unittest.main()
--- kiyoaki/processor.py
+++ kiyoaki/processor.py
@@ -4,7 +4,7 @@
 import re
 from random import choice
 
-class KBaseBlack:
+class KBaseBlock:
     def __init__(self):
         self.sub_items = []
         self.parent = None
@@ -21,25 +21,15 @@ class KBaseBlack:
     def get_parent(self):
         return self.parent
 
-class KMethod(KCodeBlock):
+class KMethod(KBaseBlock):
     def __init__(self, title):
-        KCodeBlock.__init__(self)
+        KBaseBlock.__init__(self)
         self.title=title
 
     def get_title(self):
         return self.title
 
-class KCommand():
-    def __init__(self, rule = None, return_mode = True, random_key = 0):
-        KCodeBlock.__init__(self, rule, return_mode, random_key)
-
-    def process(self, input_string):
-        if self.rule is None:
-            return input_string
-
-        return self.rule.apply(input_string)
-
-class KCodeBlock(KBaseBlock, KCommand):
+class KCodeBlock(KBaseBlock):
     def __init__(self, rule = None, return_mode = True, random_key = 0):
         KBaseBlock.__init__(self)
         self.rule = rule
@@ -47,26 +37,30 @@ class KCodeBlock(KBaseBlock, KCommand):
         self.random_key = random_key
 
     def can_be_used_for(self, input_string):
-        return self.rule is None or self.rule.test(unput_string)
+        return self.rule is None or self.rule.test(input_string)
 
-    def is_return_mode(self, return_mode):
+    def is_return_mode(self):
         return self.return_mode
 
     def get_random_key(self, key):
         return self.random_key
 
-    def is_random_select(self, key):
+    def is_random_select(self):
         return self.random_key is not None and self.random_key > 0
 
-    def CreateBlock(self, rule, return_mode, random_key):
+    def AddRule(self, rule):
+        self.rule = rule
+        return self
+
+    def CreateBlock(self, rule, return_mode = False, random_key = True):
         new_block = KCodeBlock(rule, return_mode, random_key)
         self.append(new_block)
         return new_block
 
-    def CreateCommand(self, rule, return_mode, random_key):
-        new_block = KCodeBlock(rule, return_mode, random_key)
+    def CreateCommand(self, rule, return_mode = True, random_key = 0):
+        new_block = KCommand(rule, return_mode, random_key)
         self.append(new_block)
-        return self
+        return new_block
 
     def Block(self, rule = None, return_mode = True, random_key = 0):
         return self.CreateBlock(rule, return_mode, random_key)
@@ -85,7 +79,7 @@ class KCodeBlock(KBaseBlock, KCommand):
 
     def process(self, input_string):
         blocks_to_exec = []
-        for sub_item in sub_items:
+        for sub_item in self.sub_items:
             if sub_item.can_be_used_for(input_string):
                 if sub_item.is_random_select():
                     blocks_to_exec.append(sub_item)
@@ -100,6 +94,17 @@ class KCodeBlock(KBaseBlock, KCommand):
 
         return input_string
 
+
+class KCommand(KCodeBlock):
+    def __init__(self, rule = None, return_mode = True, random_key = 0):
+        KCodeBlock.__init__(self, rule, return_mode, random_key)
+
+    def process(self, input_string):
+        if self.rule is None:
+            return input_string
+
+        return self.rule.replace(input_string)
+
 class KProcessorModulesPool(KBaseBlock):
     def __init__(self):
         KBaseBlock.__init__(self)
@@ -112,37 +117,37 @@ class KProcessorModulesPool(KBaseBlock):
         return input_string
 
 class RuleValidator:
-    CMD_COMPARE_EQ = 1
-    CMD_COMPARE_NOTEQ = 2
-    def __init__(self, compare_command, comparation_set):
-        self.comparation_set = None
-        self.compare_command = compare_command
-
-    def _get_compare_command(self):
-        return self.compare_command
+    def __init__(self, comparation_set, replacement):
+        self.comparation_set = []
+        if comparation_set is not None:
+            self.load_set(comparation_set)
+        self.replacement = replacement
 
     def test(self, input_string):
-        compare_command = self._get_compare_command()
         for comparation_item in self.comparation_set:
             if comparation_item.match(input_string):
-                return compare_command == self.CMD_COMPARE_EQ
-        return compare_command == self.CMD_COMPARE_NOTEQ
+                return True
+        return False
 
-    def replace(self, input_string, replacement):
-        if replacement is None:
-            replacement = ""
+    def replace(self, input_string):
         for comparation_item in self.comparation_set:
             if comparation_item.match(input_string):
-                return re.sub(comparation_item,  "\\1%s\\2" % replacement, input_string)
+                return re.sub(comparation_item,  "\\1%s\\2" % self.replacement, input_string)
         return None
 
-    def load_comparation_set(self, comparation_set):
-        self.comparation_set = []
+    def load_set(self, comparation_set):
         for comparation_item in comparation_set:
-            new_comparation_item = comparation_item.replace("-", "(.*)")
-            if comparation_item[0] != "-":
-                new_comparation_item = "()"+new_comparation_item
-            if comparation_item[-1] != "-":
-                new_comparation_item = new_comparation_item + "()"
-            self.comparation_set.append(re.compile(new_comparation_item))
+            self.comparation_set.append(re.compile(self.parse_comparation_item(comparation_item)))
         return self
+
+    def parse_comparation_item(self, comparation_item):
+        new_comparation_item = comparation_item.replace("-", "(.*)")
+        if comparation_item[0] != "-":
+            new_comparation_item = "()"+new_comparation_item
+        if comparation_item[-1] != "-":
+            new_comparation_item = new_comparation_item + "()"
+        return new_comparation_item
+
+
+if __name__ == "__main__":
+    print "Base classes for Kiyoaki project"
